bffd85cdef87e8f05d4de0d53fed3149eb72d171
Nicky <nickycorro@hotmail.com>
2025-08-02 16:43:56 -0400

    Update gemini-installer.sh

--- gemini-installer.sh
+++ gemini-installer.sh
@@ -2,8 +2,12 @@
 read -p "Enter your Gemini API Key: " GEMINI_API_KEY
 echo "export GEMINI_API_KEY=$GEMINI_API_KEY" >> ~/.profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v-0.39.7/install.sh | bash
+cat << 'EOF' >> ~/.profile
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+EOF
+source ~/.profile
 nvm install node
 nvm alias default node
 npm install -g @google/gemini-cli
